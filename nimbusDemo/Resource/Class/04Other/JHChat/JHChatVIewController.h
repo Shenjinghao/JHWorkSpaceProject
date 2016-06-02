3dd7e8523d7c985b45492ab6a3938e39f5493f0e
shenjinghao <shenjinghao@chunyu.me>
2016-06-02 21:06:53 +0800

    chatVC

---
+++ nimbusDemo/Resource/Class/04Other/JHChat/JHChatVIewController.h
@@ -0,0 +1,13 @@
+//
+//  JHChatVIewController.h
+//  测试Demo
+//
+//  Created by Shenjinghao on 16/6/2.
+//  Copyright © 2016年 Shenjinghao. All rights reserved.
+//
+
+#import "JHTableViewController.h"
+
+@interface JHChatVIewController : JHTableViewController
+
+@end
---
+++ nimbusDemo/Resource/Class/04Other/JHChat/JHChatVIewController.m
@@ -0,0 +1,24 @@
+//
+//  JHChatVIewController.m
+//  测试Demo
+//
+//  Created by Shenjinghao on 16/6/2.
+//  Copyright © 2016年 Shenjinghao. All rights reserved.
+//
+
+#import "JHChatVIewController.h"
+#import "uivi"
+
+@implementation JHChatVIewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = RGBCOLOR_HEX(0xfefdfd);
+    self.tableView.backgroundColor = COLOR_A9;
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    [self.tableView ];
+}
+
+@end
